d05bfd4c013ec2a038c4f0571b90eb4b7723eb96
ksrinreddy <ksrinreddy@gmail.com>
2023-05-20 12:19:24 +0530

    built app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
-function App() {
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [list, setList] = useState(getLocalStorage());
+  const [alert, setAlert] = useState({
+    show: false,
+    type: "",
+    msg: "",
+  });
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // conditions while handling submit
+    if (!name) {
+      // display alert
+      showAlert(true, "danger", "please enter value");
+    } else if (name && isEditing) {
+      // deal with editing
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          } else {
+            return item;
+          }
+        })
+      );
+      setIsEditing(false);
+      setEditID(null);
+      setName("");
+      showAlert(true, "success", "value changed");
+    } else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+      showAlert(true, "success", "item added to the list");
+    }
+  };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
+  const removeItem = (id) => {
+    setList(list.filter((item) => item.id !== id));
+    showAlert(true, "danger", "item removed");
+    // extra code to handle deleting item while editing the item
+    setIsEditing(false);
+    setName("");
+  };
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button className="submit-btn" type="submit">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button
+            className="clear-btn"
+            type="button"
+            onClick={() => {
+              setList([]);
+              showAlert(true, "danger", "empty list");
+            }}
+          >
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
   );
-}
+};
 
 export default App;
